eb3a0f2dc978d2e5377807f1329e4e815a7faed8
elliottqian <752004979@qq.com>
2016-12-05 15:19:20 +0800

    nothing

--- src/main/java/elliott/qian/sparkstreaming/SparkStreamingQuickStart.scala
+++ src/main/java/elliott/qian/sparkstreaming/SparkStreamingQuickStart.scala
@@ -7,7 +7,9 @@ import org.apache.spark.streaming._ // not necessary since Spark 1.3
   * Created by ElliottQian on 2016.04.12.
   */
 object SparkStreamingQuickStart {
+
   def main(args: Array[String]): Unit = {
+
     val conf = new SparkConf().setMaster("local").setAppName("SSQS")
     val sparkStreaming = new StreamingContext(conf, Seconds(1))
 
